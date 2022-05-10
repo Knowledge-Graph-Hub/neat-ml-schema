b5b2e6a7d813a351303aa31d4861009f2b97b011
Harshad Hegde <hegdehb@gmail.com>
2022-05-09 19:38:32 -0500

    completed fail test

--- tests/test_neat_schema.py
+++ tests/test_neat_schema.py
@@ -25,10 +25,14 @@ class TestSchema(TestCase):
     def setUp(self) -> None:
         """More setup."""
         self.schema_path = os.path.join(self.project_dir, self.SCHEMA_FILENAME)
-        self.test_yaml_1 = os.path.join(
+        self.good_yaml = os.path.join(
             self.resource_dir, self.GOOD_YAML_FILENAME
         )
+        self.bad_yaml = os.path.join(self.resource_dir, self.BAD_YAML_FILENAME)
         self.loader = YAMLLoader()
+        with open(self.schema_path, "r") as s:
+            self.sch = yaml.safe_load(s)
+        self.class_list = list(self.sch["classes"].keys())
 
     def test_schema_validation_pass(self):
         """Test validation of all classes of good YAML as per schema."""
@@ -37,33 +41,25 @@ class TestSchema(TestCase):
         python_module = PythonGenerator(schema).compile_module()
         # Get all classes
         # sv = SchemaView(schema=schema)
-        with open(self.schema_path, "r") as s:
-            sch = yaml.safe_load(s)
-        class_list = list(sch["classes"].keys())
 
-        for target_class in class_list:
+        for target_class in self.class_list:
             py_target_class = python_module.__dict__[target_class]
             obj = self.loader.load(
-                source=self.test_yaml_1, target_class=py_target_class
+                source=self.good_yaml, target_class=py_target_class
             )
             result = validator.validate_object(obj, py_target_class)
             self.assertIsNone(result)
 
-    # def test_schema_validation_fail(self):
-    #     """Test validation of a bad YAML as per schema."""
-    #     schema = self.schema_path
-    #     validator = JsonSchemaDataValidator(schema)
-    #     python_module = PythonGenerator(schema).compile_module()
-    #     # Get all classes
-    #     # sv = SchemaView(schema=schema)
-    #     with open(self.schema_path, "r") as s:
-    #         sch = yaml.safe_load(s)
-    #     class_list = list(sch["classes"].keys())
+    def test_schema_validation_fail(self):
+        """Test validation of a bad YAML as per schema."""
+        schema = self.schema_path
+        python_module = PythonGenerator(schema).compile_module()
 
-    #     for target_class in class_list:
-    #         py_target_class = python_module.__dict__[target_class]
-    #         obj = self.loader.load(
-    #             source=self.test_yaml_1, target_class=py_target_class
-    #         )
-    #         result = validator.validate_object(obj, py_target_class)
-    #         self.assertIsNone(result)
+        for target_class in self.class_list:
+            py_target_class = python_module.__dict__[target_class]
+            self.assertRaises(
+                ValueError,
+                lambda: self.loader.load(
+                    source=self.bad_yaml, target_class=py_target_class
+                ),
+            )
