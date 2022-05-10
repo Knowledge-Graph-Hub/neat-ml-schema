c0abee2bfdea857d2db287a0a7aeae6774adfb9a
Harshad Hegde <hegdehb@gmail.com>
2022-05-09 19:22:57 -0500

    successful test

--- tests/test_neat_schema.py
+++ tests/test_neat_schema.py
@@ -18,21 +18,26 @@ class TestSchema(TestCase):
         cwd = os.path.dirname(__file__)
         cls.project_dir = os.path.join(os.getcwd(), "neat_schema")
         cls.resource_dir = os.path.join(cwd, "resources")
+        cls.GOOD_YAML_FILENAME = "test_good.yaml"
+        cls.BAD_YAML_FILENAME = "test_bad.yaml"
+        cls.SCHEMA_FILENAME = "neat_schema.yaml"
 
     def setUp(self) -> None:
         """More setup."""
-        self.schema_yaml = os.path.join(self.project_dir, "neat_schema.yaml")
-        self.test_yaml_1 = os.path.join(self.resource_dir, "test.yaml")
+        self.schema_path = os.path.join(self.project_dir, self.SCHEMA_FILENAME)
+        self.test_yaml_1 = os.path.join(
+            self.resource_dir, self.GOOD_YAML_FILENAME
+        )
         self.loader = YAMLLoader()
 
-    def test_schema_validation(self):
-        """Test validation all classes as per schema."""
-        schema = self.schema_yaml
+    def test_schema_validation_pass(self):
+        """Test validation of all classes of good YAML as per schema."""
+        schema = self.schema_path
         validator = JsonSchemaDataValidator(schema)
         python_module = PythonGenerator(schema).compile_module()
         # Get all classes
         # sv = SchemaView(schema=schema)
-        with open(self.schema_yaml, "r") as s:
+        with open(self.schema_path, "r") as s:
             sch = yaml.safe_load(s)
         class_list = list(sch["classes"].keys())
 
@@ -43,3 +48,22 @@ class TestSchema(TestCase):
             )
             result = validator.validate_object(obj, py_target_class)
             self.assertIsNone(result)
+
+    # def test_schema_validation_fail(self):
+    #     """Test validation of a bad YAML as per schema."""
+    #     schema = self.schema_path
+    #     validator = JsonSchemaDataValidator(schema)
+    #     python_module = PythonGenerator(schema).compile_module()
+    #     # Get all classes
+    #     # sv = SchemaView(schema=schema)
+    #     with open(self.schema_path, "r") as s:
+    #         sch = yaml.safe_load(s)
+    #     class_list = list(sch["classes"].keys())
+
+    #     for target_class in class_list:
+    #         py_target_class = python_module.__dict__[target_class]
+    #         obj = self.loader.load(
+    #             source=self.test_yaml_1, target_class=py_target_class
+    #         )
+    #         result = validator.validate_object(obj, py_target_class)
+    #         self.assertIsNone(result)
