00f73486fb15a2803615775dc049c93d977c1404
Harshad Hegde <hegdehb@gmail.com>
2022-05-11 17:38:42 -0500

    Initial commit

--- src/NEAT_schema/datamodel/NEAT_schema.py
+++ src/NEAT_schema/datamodel/NEAT_schema.py
@@ -1,5 +1,5 @@
 # Auto generated from NEAT_schema.yaml by pythongen.py version: 0.9.0
-# Generation date: 2022-05-11T17:26:38
+# Generation date: 2022-05-11T17:38:37
 # Schema: NEAT_schema
 #
 # id: https://w3id.org/neat_schema
