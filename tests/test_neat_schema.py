a0ccf5ff842693b6cd924fa4e38836e50913dff1
Harshad Hegde <hegdehb@gmail.com>
2022-05-20 18:32:08 -0500

    fixed typo

--- tests/test_neat_schema.py
+++ tests/test_neat_schema.py
@@ -24,7 +24,7 @@ class TestSchema(TestCase):
         self.resource_dir = os.path.join(cwd, "resources")
         self.GOOD_YAML_FILENAME = "test_good.yaml"
         self.BAD_YAML_FILENAME = "test_bad.yaml"
-        self.SCHEMA_FILENAME = "NEAT_schema.yaml"
+        self.SCHEMA_FILENAME = "neat_schema.yaml"
         self.schema_path = os.path.join(self.project_dir, self.SCHEMA_FILENAME)
         self.good_yaml = os.path.join(
             self.resource_dir, self.GOOD_YAML_FILENAME
